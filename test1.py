01ecef461f693b6bf711ecfeb04cf68f3b885bca
swooperior <34080406+swooperior@users.noreply.github.com>
2019-08-07 23:26:34 +0100

    Update test1.py

--- test1.py
+++ test1.py
@@ -6,20 +6,36 @@ from block import Block
 
 evilCoin = Blockchain('EC',3,10)
 
-
-#although each block has its own transactions, all of them appear in every block...
-
-#block1 = Block([Transaction('Reece','Caryn',5)],evilCoin.get_last_block().getHash())
-#block2 = Block([Transaction('Reece','Caryn',10),Transaction('Caryn','Reece',5)],evilCoin.get_last_block().getHash())
-#block3 = Block([],evilCoin.get_last_block().getHash())
-
-#evilCoin.mine(block1)
-#evilCoin.mine(block2)
-#evilCoin.mine(block3)
-
 while True:
-    inp = input('Mine?')
-    if inp:
-        evilCoin.mine(Block([],evilCoin.get_last_block().getHash()))
+    print('Welcome to the blockchain.')
+    print('1 - Create a transaction')
+    print('2 - Mine block')
+    print('3 - View Entire blockchain')
+    print('4 - View Current block details')
+    print('q - Quit')
+
+    usr_in = input('-> ')
+
+    block = Block(None,evilCoin.get_last_block().getHash())
+    if usr_in == '1':
+        print('Creating a new transaction Record.')
+        sender = input('Sending Address: ')
+        reciever = input('Recieving Address: ')
+        amount = input('Amount: ')
+        tx = Transaction(sender,reciever,float(amount))
+        block.addTransaction(tx)
+        print('Transaction '+ tx.signature +' added to block.') 
+    elif usr_in == '2':
+        evilCoin.mine(block)
+    elif usr_in == '3':
         evilCoin.printChain()
-        evilCoin.verifyChain()
+    elif usr_in == '4':
+        block.printDetails()
+    elif usr_in == 'q':
+        break
+    else:
+        print('Invalid option.')
+        continue
+    evilCoin.verifyChain()
+else:     
+    print('Goodbye!')
